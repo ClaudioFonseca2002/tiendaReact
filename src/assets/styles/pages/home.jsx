7ba2ea811176d754dab4ce8d3c8eff7e0d1d97ce
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2025-02-22 23:56:41 -0300

    Se ha agreagdo Banner.jsx correctamente

--- src/assets/styles/pages/home.jsx
+++ src/assets/styles/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../../layouts/Headers";
 import Category from "../../../layouts/Category";
+import Banner from "../../../layouts/Banner";
 
 const Home = () => {
   return (
     <>
       <Header></Header>
       <Category></Category>
+      <Banner></Banner>
     </>
   );
 };
---
+++ src/layouts/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../assets/images/banner.jpg";
+
+const Category = () => {
+  return (
+    <div className="banner">
+      <img src={banner} alt="Banner promocional" className="bannerImage" />
+    </div>
+  );
+};
+
+export default Category;
