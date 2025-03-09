74ceeb9219a7e873025cb84b16e33e60d51981d9
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2025-03-09 12:13:31 -0300

    Se han realizado cambios en Brands.jsx

--- src/layouts/Brands.jsx
+++ src/layouts/Brands.jsx
@@ -1,25 +1,53 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper/modules";
+
 import penaltie from "../assets/images/brands/penaltie.png";
 import jhonfoes from "../assets/images/brands/jhonfoes.png";
 import kappa from "../assets/images/brands/kappa.png";
 import rebook from "../assets/images/brands/rebook.png";
 import adidas from "../assets/images/brands/adidas.png";
 import topper from "../assets/images/brands/topper.png";
+import puma from "../assets/images/brands/puma.png";
 
-const Brands = () =>{
-    return <>
-        <div className="containerBrands">
-            <div className="containerText">Marcas</div>
-            <div className="containerLogos">
-                <img src={penaltie} alt="Logo Penaltie" />
-                <img src={jhonfoes} alt="Logo JhonFoes" />
-                <img src={kappa} alt="Logo Kappa" />
-                <img src={rebook} alt="Logo Rebook" />
-                <img src={adidas} alt="Logo Adidas" />
-                <img src={topper} alt="Logo topper" />
-            </div>
-        </div>
-    </>
-}
+const Brands = () => {
+  const brands = [
+    { src: penaltie, alt: "Penaltie" },
+    { src: jhonfoes, alt: "JhonFoes" },
+    { src: kappa, alt: "Kappa" },
+    { src: rebook, alt: "Rebook" },
+    { src: adidas, alt: "Adidas" },
+    { src: topper, alt: "Topper" },
+    { src: puma, alt: "Puma" },
+  ];
+
+  return (
+    <div className="containerBrands">
+      <div className="containerText">Marcas</div>
+      <Swiper
+        slidesPerView={6} // Cantidad visible en pantallas grandes
+        spaceBetween={150} // Espaciado entre logos
+        freeMode={true}
+        pagination={{ clickable: true }}
+        modules={[FreeMode, Pagination]}
+        breakpoints={{
+          320: { slidesPerView: 3 }, // Para móviles
+          768: { slidesPerView: 4 }, // Tablets
+          1024: { slidesPerView: 4 }, // Desktop
+        }}
+        className="containerLogos"
+      >
+        {brands.map((brand, index) => (
+          <SwiperSlide key={index}>
+            <img src={brand.src} alt={brand.alt} className="brandLogo" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
 export default Brands;
